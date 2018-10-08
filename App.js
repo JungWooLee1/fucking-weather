ab339b96770c12796a28c409f35defa10e993979
Jungwoo Lee <jjjjjw910911@letsee.io>
2018-10-08 16:44:37 +0900

    first commit

---
+++ App.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import Weather from "./Weather"
+
+export default class App extends React.Component {
+
+  state = {
+    isLoaded: false,
+    error:null
+  };
+
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        console.log(position);
+        this.setState({
+          isLoaded:true
+        })
+      },
+      error => {
+        console.log(error);
+        this.setState({
+          error: error
+        })
+      }
+    );
+  }
+
+  render() {
+    const { isLoaded, error } = this.state;
+
+    return (
+      <View style={styles.container}>
+
+        <StatusBar hidden={true}/>
+        {isLoaded ? (
+          <Weather/>
+        ) :(
+          <View style={styles.loading}>
+            <Text style={styles.loadingText}>Getting the fucking weather</Text>
+            {error ? <Text style={styles.errorText}> {error} </Text> : null}
+
+          </View>
+        )}
+      </View>
+    );
+  }
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor:'#fff',
+  },
+  errorText:{
+    color:"red",
+    backgroundColor:"transparent",
+    marginBottom:40
+  }
+  ,
+  loading : {
+    flex:1,
+    backgroundColor: '#FDF6AA',
+    justifyContent:"flex-end",
+    paddingLeft:25
+  },
+  loadingText: {
+    fontSize:38,
+    marginBottom:24
+  }
+});
+
